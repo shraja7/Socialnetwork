dff42804d7f8942dca3b80636a639186992e0027
shraja7 <shraja7@gmail.com>
2022-12-13 09:46:58 -0600

    initial commit

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import exports from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = exports.Router();
+
+//read route
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+//update route
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
